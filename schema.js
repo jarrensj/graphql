946702f9e545ab95f5096f9ba2a33991331a3e81
Jarren San Jose <sanjose.jarren@gmail.com>
2018-12-22 15:55:42 -0800

    graphql

---
+++ schema.js
@@ -0,0 +1,51 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql');
+
+// hardcoded data
+const customers = [
+  {id: '1', name:'John Doe', email: 'jdoe@gmail.com', age:21},
+  {id: '2', name:'Bob', email: 'bob@gmail.com', age:22},
+  {id: '3', name:'Steve', email: 'Steve@gmail.com', age:23}
+]
+
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: {type:GraphQLString},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    age: {type: GraphQLInt}
+  })
+})
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type:CustomerType,
+      args: {
+        id:{type: GraphQLString}
+      },
+      resolve(parentValue, args) {
+        for(let i = 0; i < customers.length; ++i) {
+          if(customers[i].id == args.id) {
+            return customers[i]
+          }
+        }
+
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
